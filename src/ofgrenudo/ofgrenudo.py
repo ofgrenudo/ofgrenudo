1bb6f67065e96218cc6d3841853994665d11d474
Joshua Winters-Brown <jwintersbro@kvcc.edu>
2026-02-25 10:51:19 -0500

    feat: added discord

--- src/ofgrenudo/ofgrenudo.py
+++ src/ofgrenudo/ofgrenudo.py
@@ -4,6 +4,7 @@ import datetime
 def main():
     print("Hello World")
     print(f"Today is {datetime.date.today()}")
+    print("You can reach me @grenudo on Discord")
 
 
 if __name__ == "__main__":
