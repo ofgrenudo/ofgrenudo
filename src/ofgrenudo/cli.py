59a36466b05d489137a3e71fc597c39a806bb801
Joshua Winters-Brown <jwintersbro@kvcc.edu>
2026-02-25 11:08:40 -0500

    add argparse, and bump version

--- src/ofgrenudo/cli.py
+++ src/ofgrenudo/cli.py
@@ -1,7 +1,17 @@
+import argparse
 import datetime
+import importlib.metadata
 
 
 def main() -> None:
+    parser = argparse.ArgumentParser(prog="ofgrenudo")
+    parser.add_argument("--version", action="store_true", help="Show version")
+    args = parser.parse_args()
+
+    if args.version:
+        print(importlib.metadata.version("ofgrenudo"))
+        return
+
     print("Hello World")
     print(f"Today is {datetime.date.today()}")
     print("You can reach me @grenudo on Discord")
